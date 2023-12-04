e6978316eddf40b26bdb7fb998eda4b1a4fb00d9
Rick O'Sullivan <rick.osullivan@patrodyne.org>
2023-12-04 12:48:12 -0500

    Add "exec.args" to run script.

--- assembly/samples/resourced/bin/run.sh
+++ assembly/samples/resourced/bin/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# Run a Maven goal to execute the application.
+
 if grep -q "<packaging>jar</packaging>" pom.xml; then
 	BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 	source ${BASEDIR}/build-cfg.sh
 	source ${BASEDIR}/build-inc.sh
 	export MAVEN_OPTS="${MAVEN_OPTS} ${JVM_SYS_PROPS}"
 
-	mvn ${MAVEN_ARGS} -Pexec clean compile exec:java
+	mvn ${MAVEN_ARGS} -Pexec clean compile exec:java -Dexec.args="$@"
 else
     echo "Not a JAR project!"
 fi
